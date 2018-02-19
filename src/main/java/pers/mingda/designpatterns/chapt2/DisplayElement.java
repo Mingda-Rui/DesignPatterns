92e05ee2c96cc416b6b6cd491b26f7879559408a
Mingda Rui <mingdarui@gmail.com>
2018-02-19 15:48:39 -0800

    Chapt 2 Observer Patterns All interfaces

---
+++ src/main/java/pers/mingda/designpatterns/chapt2/DisplayElement.java
@@ -0,0 +1,8 @@
+package pers.mingda.designpatterns.chapt2;
+
+// The DisplayElement interface just includes
+// one method, display(), that we will call when
+// the dispaly element needs to be displayed.
+public interface DisplayElement {
+    public void display();
+}
---
+++ src/main/java/pers/mingda/designpatterns/chapt2/Observer.java
@@ -0,0 +1,16 @@
+package pers.mingda.designpatterns.chapt2;
+
+// The Observer interface is
+// implemented by all observers,
+// so they all have to implement
+// the update() method. Here
+// we're following Mary and
+// Sue's lead and passing the
+// measurements to the observers.
+public interface Observer {
+
+    // These are the state values the Observers get from
+    // the Subject when a weather measurement changes
+    public void update(float temp, float humidity, float pressure);
+
+}
