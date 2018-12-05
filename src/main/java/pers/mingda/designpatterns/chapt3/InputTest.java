b3f0c74534b674344438df13764456d4483d9977
Mingda Rui <mingdarui@gmail.com>
2018-12-05 00:25:04 +0000

    Decorator Pattern in JavaIO Example

---
+++ src/main/java/pers/mingda/designpatterns/chapt3/InputTest.java
@@ -0,0 +1,28 @@
+package pers.mingda.designpatterns.chapt3;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import pers.mingda.designpatterns.chapt3.LowerCaseInputStream;
+
+public class InputTest {
+    public static void main(String[] args) throws IOException {
+        int c;
+        try {
+            InputStream in = new LowerCaseInputStream(new BufferedInputStream(new FileInputStream("test.txt")));
+
+            while((c = in.read()) >= 0) {
+                System.out.print((char)c);
+            }
+
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+    // public static void main(String[] args) {
+    //     System.out.println("workdin directory = " + System.getProperty("user.dir"));
+    // }
+}
