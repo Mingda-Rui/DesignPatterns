c9b77d1e0978c1882ff92faae1fb0fd5b9e60dff
Mingda Rui <mingdarui@gmail.com>
2019-03-16 14:43:23 +0000

    pizza v2

--- src/main/java/pers/mingda/designpatterns/chapt4/Pizza.java
+++ src/main/java/pers/mingda/designpatterns/chapt4/Pizza.java
@@ -1,17 +1,31 @@
 package pers.mingda.designpatterns.chapt4;
 
 public class Pizza {
-    Pizza orderPizza() {
-        // For flexibility, we really want 
-        // this to be an abstract class or
-        // interface, but we can't directly
-        // instantiate either of those.
-        Pizza pizza = new Pizza();
+    Pizza orderPizza(
+        String type /* We're now passing in the type of pizza to orderPizza */) {
 
-        // pizza.prepare();
-        // pizza.bake();
-        // pizza.cut();
-        // pizza.box();
+        Pizza pizza;
+
+        // Based on the type of pizza, we 
+        // instantiate the correct contrete class
+        // and assign it to the pizza instance
+        // variable. Note that each pizza here
+        // has to implement the Pizza interface.
+        if (type.equals("cheese")) {
+            pizza = new CheesePizza();
+        } else if (type.equals("greek")) {
+            pizza = new GreekPizza();
+        } else if (type.equals("pepperoni")) {
+            pizza = new PepperoniPizza();
+        }
+
+        // Each Pizza subtype (CheesPizza,
+        // VeggiePizza, etc.) knows how to 
+        // prepare itself.
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
         return pizza;
     }
 }
