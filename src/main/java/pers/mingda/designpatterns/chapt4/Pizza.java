0d776f572443314a4afba03899ee1920e9ebaa6c
Mingda Rui <mingdarui@gmail.com>
2019-03-25 22:00:45 +0000

    Pizza Interface V2

--- src/main/java/pers/mingda/designpatterns/chapt4/Pizza.java
+++ src/main/java/pers/mingda/designpatterns/chapt4/Pizza.java
@@ -1,8 +1,47 @@
 package pers.mingda.designpatterns.chapt4;
 
-public interface Pizza {
-    public void prepare();
-    public void bake();
-    public void cut();
-    public void box();
+import java.util.ArrayList;
+
+// We'll start with an abstract 
+// Pizza class and all the concrete
+// pizzas will derive from this.
+public abstract class Pizza {
+    // Each Pizza has a name, a type of dough, a
+    // type of sauce, and a set of toppings.    
+    String name;
+    String dough;
+    String sauece;
+    ArrayList toppings = new ArrayList();
+
+    // The abstract class provides 
+    // some basic defaults for baking,
+    // cutting and boxing.    
+    void prepare() {
+        // Preparation follows a 
+        // number of steps in a 
+        // particular sequence.     
+        System.out.println("Preparing " + name);    
+        System.out.println("Tossing dough...");
+        System.out.println("Adding sauce...");
+        System.out.println("Adding toppings: ");
+        for (int i = 0; i < toppings.size(); i++) {
+            System.out.println("   " + toppings.get(i) );
+        }
+    }
+    
+    void bake() {
+        System.out.println("Bake for 25 minutes at 350");    
+    }
+
+    void cut() {
+        System.out.println("Cutting the pizza into diagonal slices");    
+    }
+
+    void box() {
+        System.out.println("Place pizza in official PizzaStore box");
+    }    
+
+    public String getName() {
+        return name;    
+    }
 }
