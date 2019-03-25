3bfa7c729e36f87ecaa7fd13939195547f30f191
Mingda Rui <mingdarui@gmail.com>
2019-03-25 22:17:15 +0000

    Pizza Test

---
+++ src/main/java/pers/mingda/designpatterns/chapt4/PizzaTestDrive.java
@@ -0,0 +1,20 @@
+package pers.mingda.designpatterns.chapt4;
+
+public class PizzaTestDrive {
+
+    public static void main(String[] args) {
+        // First we create two 
+        // different stores.    
+        PizzaStore nyStore = new NYPizzaStore();
+        PizzaStore chicagoStore = new ChicagoPizzaStore(); 
+
+        // Then use one one store
+        // to make Ethan's order        
+        Pizza pizza = nyStore.orderPizza("cheese");
+        System.out.println("Ethan ordered a " + pizza.getName() + "\n");
+
+        pizza = chicagoStore.orderPizza("cheese");
+        System.out.println("Joel ordered a " + pizza.getName() + "\n");
+        
+    }
+}
