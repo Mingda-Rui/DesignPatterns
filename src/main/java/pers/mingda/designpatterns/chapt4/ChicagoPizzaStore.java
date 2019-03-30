1073a453134eb8cb482b4822cd735690e3aff458
Mingda Rui <mingdarui@gmail.com>
2019-03-30 15:01:29 +0000

    ChicagoPizzaStore v2

--- src/main/java/pers/mingda/designpatterns/chapt4/ChicagoPizzaStore.java
+++ src/main/java/pers/mingda/designpatterns/chapt4/ChicagoPizzaStore.java
@@ -2,4 +2,17 @@ package pers.mingda.designpatterns.chapt4;
 
 public class ChicagoPizzaStore extends PizzaStore {
 
+    @Override
+    protected Pizza createPizza(String item) {
+        if (item.equals("cheese")) {
+            return new ChicagoStyleCheesePizza();
+        } else if (item.equals("veggie")) {
+            return new ChicagoStyleVeggiePizza();    
+        } else if (item.equals("clam")) {
+            return new ChicagoStyleClamPizza();    
+        } else if (item.equals("pepperoni")) {
+            return new ChicagoStylePepperoniPizza();    
+        } else return null;
+    }
+
 }
