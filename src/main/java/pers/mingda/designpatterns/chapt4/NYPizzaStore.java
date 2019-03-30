dc34aae4f46345b4be197a7268d42c9784d1cdc8
Mingda Rui <mingdarui@gmail.com>
2019-03-30 13:13:56 +0000

    New York Pizza Store v2

--- src/main/java/pers/mingda/designpatterns/chapt4/NYPizzaStore.java
+++ src/main/java/pers/mingda/designpatterns/chapt4/NYPizzaStore.java
@@ -1,15 +1,37 @@
 package pers.mingda.designpatterns.chapt4;
 
 public class NYPizzaStore extends PizzaStore {
-    Pizza createPizza(String item) {
+    protected Pizza createPizza(String item) {
+        Pizza pizza = null;
+        // The NY Store is composed with a NY
+        // pizza ingredient factory. This will
+        // be used to produce the ingredients 
+        // for al NY style pizzas.
+        PizzaIngredientFactory ingredientFactory = new NYPizzaIngredientFactory();
+                
         if (item.equals("cheese")) {
-            return new NYStyleCheesePizza();
+            
+            // We now pass each pizza the 
+            // factory that should be used to 
+            // produce its ingredients.            
+            pizza = new CheesePizza(ingredientFactory);
+            pizza.setName("New York Style Cheese Pizza");
+
         } else if (item.equals("veggie")) {
-            return new NYStyleVeggiePizza();
+            
+            pizza = new VeggiePizza(ingredientFactory);
+            pizza.setNmae("New York Style Veggie Pizza");
+
         } else if (item.equals("clam")) {
-            return new NYStyleClamPizza();
+            
+            pizza = new ClamPizza(ingredientFactory);
+            pizza.setName("New York Style Clam Pizza");
+
         } else if (item.equals("pepperoni")) {
-            return new NYStylePepperoniPizza();
-        } else return null;
+            pizza = new PepperoniPizza(ingredientFactory);
+            pizza.setNmae("New York Sytle Pepperoni Pizza");
+
+        }
+        return pizza;
     }
 }
