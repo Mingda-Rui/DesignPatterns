afa275000feb9ee57fa61a39e64bd4e680f68028
Mingda Rui <mingdarui@gmail.com>
2019-03-30 13:04:23 +0000

    CheesePizza

---
+++ src/main/java/pers/mingda/designpatterns/chapt4/CheesePizza.java
@@ -0,0 +1,26 @@
+package pers.mingda.designpatterns.chapt4;
+
+public class CheesePizza extends Pizza {
+    PizzaIngredientFactory ingredientFactory; 
+    
+    // To make a pizza now, we need
+    // a factory to procide the 
+    // ingredients. So each Pizza
+    // class gets a factory passed
+    // into its constructor, and it's
+    // stored in an instance variable    
+    public CheesePizza(PizzaIngredientFactory ingredientFactory) {
+        this.ingredientFactory = ingredientFactory;    
+    }    
+
+    void prepare() {
+        // The prepare() method steps through creating
+        // a cheese pizza, and each time it needs an
+        // ingredient, it asks the factory to produce it    
+        System.out.println("Preparing " + name);
+        dough = ingredientFactory.createDough();
+        sauce = ingredientFactory.createSauce();
+        cheese = ingredientFactory.createCheese();    
+    }    
+    
+}
