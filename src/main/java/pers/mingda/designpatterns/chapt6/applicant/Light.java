15344f58898b625e325eb70bc4f3f6828591bb87
Mingda Rui <mingdarui@gmail.com>
2019-04-06 15:05:17 +0100

    Mock Lighter

---
+++ src/main/java/pers/mingda/designpatterns/chapt6/applicant/Light.java
@@ -0,0 +1,6 @@
+package pers.mingda.designpatterns.chapt6.applicant;
+
+public class Light {
+    public void on() {System.out.println("Light On");}
+    public void off() {System.out.println("Light Off");}
+}
