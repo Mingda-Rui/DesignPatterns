31ac04216227a9f0d84e72d66c1a692c2e66c8cb
Mingda Rui <mingdarui@gmail.com>
2019-04-06 16:16:41 +0100

    Mock Graage door

---
+++ src/main/java/pers/mingda/designpatterns/chapt6/applicant/GarageDoor.java
@@ -0,0 +1,9 @@
+package pers.mingda.designpatterns.chapt6.applicant;
+
+public class GarageDoor {
+    public void up() { System.out.println("Garage door is up"); }
+    public void down() { System.out.println("Garage door is down"); }
+    public void stop() { System.out.println("Garage door is stop"); }
+    public void lightOn() { System.out.println("Garage door light is no"); }
+    public void lightOff() { System.out.println("Garage door light is off"); }
+}
