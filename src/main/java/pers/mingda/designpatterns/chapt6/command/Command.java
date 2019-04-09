8fde1b59d83dd4fd9449195f8e8b6af175009e93
Mingda Rui <mingdarui@gmail.com>
2019-04-09 23:00:01 +0100

    command with undo function

--- src/main/java/pers/mingda/designpatterns/chapt6/command/Command.java
+++ src/main/java/pers/mingda/designpatterns/chapt6/command/Command.java
@@ -3,4 +3,6 @@ package pers.mingda.designpatterns.chapt6.command;
 public interface Command {
     // Simple. All we need is one method called execute().
     public void execute();
+    // Here's the new undo() method
+    public void undo();
 }
