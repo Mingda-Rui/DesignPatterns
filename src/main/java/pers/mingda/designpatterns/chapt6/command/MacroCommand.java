4e6c8b98b727061c8ff32d6c84489fbd393876a4
Mingda Rui <mingdarui@gmail.com>
2019-04-13 14:12:51 +0100

    MacroCommand

---
+++ src/main/java/pers/mingda/designpatterns/chapt6/command/MacroCommand.java
@@ -0,0 +1,19 @@
+package pers.mingda.designpatterns.chapt6.command;
+
+public class MacroCommand implements Command {
+    Command[] commands;
+
+    public MacroCommand(Command[] commands) {
+        // Take an array of 
+        // Commands and store them in the MacroCommand.
+        this.commands = commands;
+    }
+
+    public void execute() {
+        for (int i = 0; i < commands.length; i++) {
+            // When the macro gets executed by the remote,
+            // execute those commands one at a time.
+            commands[i].execute();
+        }
+    }
+}
