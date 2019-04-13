d14f3f22be7311a6cb254c6bc1580402f96c8160
Mingda Rui <mingdarui@gmail.com>
2019-04-13 13:58:48 +0100

    CeilingFanMediumCommand

---
+++ src/main/java/pers/mingda/designpatterns/chapt6/command/CeilingFanMediumCommand.java
@@ -0,0 +1,41 @@
+package pers.mingda.designpatterns.chapt6.command;
+
+import pers.mingda.designpatterns.chapt6.applicant.CeilingFan;
+
+public class CeilingFanMediumCommand implements Command {
+    CeilingFan ceilingFan;
+    int prevSpeed;
+
+    // We've added local state
+    // to keep track of the
+    // previous speed of the fan.
+    public CeilingFanMediumCommand(CeilingFan ceilingFan) {
+        this.ceilingFan = ceilingFan;
+    }
+
+    public void execute() {
+        // In execute, before we change 
+        // the speed of the fan, we
+        // need to first record its
+        // previous state, just in case we
+        // need to undo our actions.
+        prevSpeed = ceilingFan.getSpeed();
+        ceilingFan.medium();
+    }
+
+    // To undo, we set the speed
+    // of the fan back to its
+    // previous speed.
+    public void undo() {
+        if (prevSpeed == CeilingFan.HIGH) {
+            ceilingFan.high();
+        } else if (prevSpeed == CeilingFan.MEDIUM) {
+            ceilingFan.medium();
+        } else if (prevSpeed == CeilingFan.LOW) {
+            ceilingFan.low();
+        } else if (prevSpeed == CeilingFan.OFF) {
+            ceilingFan.off();
+        }
+    }
+
+}
