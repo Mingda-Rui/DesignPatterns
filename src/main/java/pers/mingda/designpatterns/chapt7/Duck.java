b4449e6eeb500016681801b155feda8127891b5c
Mingda Rui <mingdarui@gmail.com>
2019-04-15 23:27:38 +0100

    Duck interface for Apater pattern demo

---
+++ src/main/java/pers/mingda/designpatterns/chapt7/Duck.java
@@ -0,0 +1,10 @@
+package pers.mingda.designpatterns.chapt7;
+
+// This time around, out
+// ducks implement a Duck
+// interface that allows
+// Ducks to quack and fly.
+public interface Duck {
+    public void quack();
+    public void fly();
+}
