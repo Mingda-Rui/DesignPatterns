d22e16119ac46a09a6427e1ed405e3882fc1b73c
Mingda Rui <mingdarui@gmail.com>
2019-04-16 23:26:27 +0100

    Turkey interface

---
+++ src/main/java/pers/mingda/designpatterns/chapt7/Turkey.java
@@ -0,0 +1,10 @@
+package pers.mingda.designpatterns.chapt7;
+
+public interface Turkey {
+    // Turkeys don't quack, they gobble.
+    public void gobble();
+    
+    // Turkeys can fly, although they
+    // can only fly short distances.
+    public void fly();
+}
