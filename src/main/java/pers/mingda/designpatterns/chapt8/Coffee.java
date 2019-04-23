11e10d15cb0e5f40192ceda7a870b54e5238537b
Mingda Rui <mingdarui@gmail.com>
2019-04-23 21:50:20 +0100

    Coffee V2 extends CaffeientBeverage

--- src/main/java/pers/mingda/designpatterns/chapt8/Coffee.java
+++ src/main/java/pers/mingda/designpatterns/chapt8/Coffee.java
@@ -1,39 +1,16 @@
 package pers.mingda.designpatterns.chapt8;
 
 // Here's our Coffee class for making coffee.
-public class Coffee {
+public class Coffee extends CaffeineBeverage {
     
-    // Here's our recipe for coffee,
-    // straight out of the training manual.
-    void prepareRecipe() {
-        // Each of the steps is implemented as
-        // a separate method.
-        boilWater();
-        brewCoffeeGrinds();
-        pourInCup();
-        addSugarAndMilk();
-    }
-
-    // Each of there methods
-    // implements one step of
-    // the algorithm. There's 
-    // a method to boil water,
-    // brew the coffee, pour
-    // the coffee in a cup and 
-    // add sugar and milk.
-    public void boilWater() {
-        System.out.println("Boiling water");
-    }
-
-    public void brewCoffeeGrinds() {
+    // Same for Coffee, except Coffee deals
+    // with coffee, and sugar and milk instead
+    // of tea bags and lemon.
+    public void brew() {
         System.out.println("Dripping Coffee through filter");
     }
 
-    public void pourInCup() {
-        System.out.println("Pouring into cup");
-    }
-
-    public void addSugarAndMilk() {
+    public void addCondiments() {
         System.out.println("Adding Sugar and Milk");
     }
 }
