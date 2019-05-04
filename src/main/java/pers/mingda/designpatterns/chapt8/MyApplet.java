3f161834094416200074979192d5539171e6adcf
Mingda Rui <mingdarui@gmail.com>
2019-05-04 14:52:16 +0100

    MyApplet for demonstrating Template Pattern

---
+++ src/main/java/pers/mingda/designpatterns/chapt8/MyApplet.java
@@ -0,0 +1,43 @@
+package pers.mingda.designpatterns.chapt8;
+
+import java.awt.Graphics;
+
+public class MyApplet extends Applet {
+    String message;
+
+    // The init hook allows the applet to do whatever
+    // it wants to initialize the applet the first time.
+    public void init() {
+        message = "Hello World, I'm alive!";
+        // repaint() is a concrete method in the Applet
+        // class that lets upper-level components know
+        // the applet needs to be redrawn.
+        repaint();
+    }
+
+    // The start hook allows the applet to do
+    // something when the applet is just about
+    // to be displayed on the web page.
+    public void start() {
+        message = "Now I'm starting up...";
+        repaint();
+    }
+
+    // And the destroy nook is neds when the applet
+    // is goint to be destroyed, say, when the browser
+    // pane is closed. We could try to display 
+    // somthing there, but what would be the point?
+    public void destroy() {
+        // applet is going away...
+    }
+
+    // Well looky here! Our old friend the 
+    // paint() method! Applet also makes
+    // use of this method as a hook.
+    public void paint(Graphics g) {
+        g.drawString(message, 5, 15);
+    }
+
+    // Place holder
+    private void repaint() {}
+}
