cac45bf012f78af3e9f1e0ca5c917c354cc7dc80
Mingda Rui <mingdarui@gmail.com>
2019-05-05 16:56:19 +0100

    Mel's implementation

---
+++ src/main/java/pers/mingda/designpatterns/chapt9/DinerMenu.java
@@ -0,0 +1,53 @@
+package pers.mingda.designpatterns.chapt9;
+
+// And here's Mel's implementation of the Diner menu
+public class DinerMenu {
+    static final int MAX_ITEMS = 6;
+    int numberOfItems = 0;
+    // Mel takes a different approach; he's using an Array so he
+    // can control the max size of the menu and retrieve menu
+    // items out without having to cast his objects.
+    MenuItem[] menuItems;
+
+    public DinerMenu() {
+        menuItems = new MenuItem[MAX_ITEMS]; 
+
+        addItem("Vegetarian BLT",
+            "(Fakin') Bacon with lettuce & tomato on whole wheat", true, 2.99);
+        addItem("BLT",
+            "Bacon with lettuce & tomato on whole wheat", false, 2.99);
+        addItem("Soup of the day", 
+            "Soup of the day, with a side of potato salas", false, 3.29);
+        addItem("Hotdog",
+            "A hot dog, with saurkraut, relish, onions, topped with cheese",
+            false, 3.05);
+        // a couple of other Diner Menu items added here
+    }
+
+    // addItem() takes all the parameters 
+    // necessary to create a MenuItem and 
+    // instantiates one. It also checks to make
+    // sure we haven't hit hte menu size limit.
+    public void addItem(String name, String description, boolean vegetarian, double price) {
+        MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+        if (numberOfItems >= MAX_ITEMS) {
+            // Mel specifically wnats to keep his menu under a 
+            // certain size (presumably so he doesn't have to
+            // remember too many recipes).
+            System.out.println("Sorry, menu is full! Can't add item to menu");
+        } else {
+            menuItems[numberOfItems] = menuItem;
+            numberOfItems = numberOfItems + 1;
+        }
+    }
+
+    // getMenuitems() returns the array of menu items.
+    public MenuItem[] getMenuItems() {
+        return menuItems;
+    }
+
+    // other menu methods here 
+    // Like Lou, Mel has a bunch of code that depends on the implementation of 
+    // his menu being an Array. He's too busy cooking to rewrite all of this.
+
+}
