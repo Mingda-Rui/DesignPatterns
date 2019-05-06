5f77a50d8b9ba7a21cc0acba1753c4cbe8c71b8e
Mingda Rui <mingdarui@gmail.com>
2019-05-06 21:18:37 +0100

    Waitress V2 with new CafeMenu

--- src/main/java/pers/mingda/designpatterns/chapt9/Waitress.java
+++ src/main/java/pers/mingda/designpatterns/chapt9/Waitress.java
@@ -5,12 +5,14 @@ import java.util.Iterator;
 public class Waitress {
     Menu pancakeHouseMenu;
     Menu dinerMenu;
+    Menu cafeMenu;
 
     // In the constructor the Waitres
     // takes the two menus.
-    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu) {
+    public Waitress(Menu pancakeHouseMenu, Menu dinerMenum, Menu cafeMenu) {
         this.pancakeHouseMenu = pancakeHouseMenu;
         this.dinerMenu = dinerMenu;
+        this.cafeMenu = cafeMenu;
     }
 
     // The printMenu() method now creates two
@@ -18,6 +20,7 @@ public class Waitress {
     public void printMenu() {
         Iterator<MenuItem> pancakeIterator = pancakeHouseMenu.createIterator();
         Iterator<MenuItem> dinerIterator = dinerMenu.createIterator();
+        Iterator<MenuItem> cafeIterator = cafeMenu.createIterator();
         // And then calls the
         // overloaded printMenu()
         // with each iterator.
@@ -25,9 +28,17 @@ public class Waitress {
         printMenu(pancakeIterator);
         System.out.println("\nLUNCH");
         printMenu(dinerIterator);
+        // We're using the Cafe's 
+        // menu for our dinner menu.
+        // All we have to do to print
+        // it is create the iterator,
+        // and pass it to printMenu().
+        // That's it!
+        System.out.println("\nDINNER");
+        printMenu(cafeIterator);
     }
 
-    private void printMenu(Iterator iterator) {
+    private void printMenu(Iterator<MenuItem> iterator) {
         // Test if there are any more items.
         while(iterator.hasNext()) {
             // Get the next item.
