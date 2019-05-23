164cf3aa83b051bf9d21951e2f1397638320a6b4
Mingda-Rui <mingdarui@gmail.com>
2019-05-23 22:20:27 +0100

    CafeMenu extends new Menu class

--- src/main/java/pers/mingda/designpatterns/chapt9/CafeMenu.java
+++ src/main/java/pers/mingda/designpatterns/chapt9/CafeMenu.java
@@ -6,18 +6,22 @@ import java.util.Iterator;
 // CafeMenu implements the Menu
 // interface, so the Waitress can use
 // it just like the other two Menus.
-public class CafeMenu implements Menu {
+public class CafeMenu extends Menu {
     // The Cafe is storing their menu items in a Hashtable.
     // Does that support Iterator? We'l see shortly...
     HashMap<String, MenuItem> menuItems = new HashMap<>();
 
-    public CafeMenu() {
-        // Like the other Menus, the menu items are
-        // initialized in the constructor.
-        addItem("Veggie Burger and Air Fries", "Veggie burger on a whole wheat bun, lettuce, tomato, and fries", true,
-                3.99);
-        addItem("Soup of the day", "A cup of the soup of the day, with a side salad", false, 3.69);
-        addItem("Burrito", "A large burrito, with whole pinto beans, salsa, guacamole", true, 4.29);
+    // public CafeMenu() {
+    //     // Like the other Menus, the menu items are
+    //     // initialized in the constructor.
+    //     addItem("Veggie Burger and Air Fries", "Veggie burger on a whole wheat bun, lettuce, tomato, and fries", true,
+    //             3.99);
+    //     addItem("Soup of the day", "A cup of the soup of the day, with a side salad", false, 3.69);
+    //     addItem("Burrito", "A large burrito, with whole pinto beans, salsa, guacamole", true, 4.29);
+    // }
+
+    public CafeMenu(String name, String description) {
+        super(name, description);
     }
 
     // Here's where we create a new MenuItem
@@ -37,7 +41,6 @@ public class CafeMenu implements Menu {
     // And here's where we implement the createIterator()
     // method. Notice that we're not getting an Iterator
     // for the whole HashMap, just for the values.
-    @Override
     public Iterator<MenuItem> createIterator() {
         return menuItems.values().iterator();
     }
