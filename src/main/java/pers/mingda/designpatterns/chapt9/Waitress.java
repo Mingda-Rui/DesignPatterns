a68edd9b6f2b1f1b1754480d03869a4e810e31d0
Mingda-Rui <mingdarui@gmail.com>
2019-05-23 21:23:50 +0100

    Waitress V2 uptake recursive print

--- src/main/java/pers/mingda/designpatterns/chapt9/Waitress.java
+++ src/main/java/pers/mingda/designpatterns/chapt9/Waitress.java
@@ -1,44 +1,20 @@
 package pers.mingda.designpatterns.chapt9;
 
-import java.util.Iterator;
-import java.util.ArrayList;
-
+// Yup! The Waitress code really is this simple.
+// Now we just hand her the top level menu
+// component, the one that contains all the
+// other menus. We've called that allMenus.
 public class Waitress {
-    // Now we just take an
-    // ArrayList of menus.
-    ArrayList<Menu> menus;
-    
+    MenuComponent allMenus;
 
-    // In the constructor the Waitres
-    // takes the two menus.
-    public Waitress(ArrayList<Menu> menus) {
-        this.menus = menus;
+    public Waitress(MenuComponent allMenus) {
+        this.allMenus = allMenus;
     }
 
-    // And we iterate through the menus,
-    // passing each menu's iterator to 
-    // the overloaded printMenu() method.
+    // All she has to do to print the entire menu
+    // hierarchy - all the menus, and all the menu
+    // items - is call print() on the top level menu.
     public void printMenu() {
-        Iterator<Menu> menuIterator = menus.iterator();
-        while (menuIterator.hasNext()) {
-            Menu menu = (Menu)menuIterator.next();
-            printMenu(menu.createIterator());
-        }   
+        allMenus.print();
     }
-
-    private void printMenu(Iterator<MenuItem> iterator) {
-        // Test if there are any more items.
-        while(iterator.hasNext()) {
-            // Get the next item.
-            MenuItem menuItem = (MenuItem)iterator.next();
-            // Use the item to get name, price
-            // and description and print them.
-            System.out.println(menuItem.getName() + ", ");
-            System.out.println(menuItem.getPrice() + " -- ");
-            System.out.println(menuItem.getDescription());
-        }
-    }
-
-    // other methods here
-    
 } 
