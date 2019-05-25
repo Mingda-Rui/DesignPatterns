d06348b69511e9e33213076413f08e8a2f17dd2b
Mingda-Rui <mingdarui@gmail.com>
2019-05-25 11:14:15 +0100

    Waitress V3

--- src/main/java/pers/mingda/designpatterns/chapt9/Waitress.java
+++ src/main/java/pers/mingda/designpatterns/chapt9/Waitress.java
@@ -1,9 +1,7 @@
 package pers.mingda.designpatterns.chapt9;
 
-// Yup! The Waitress code really is this simple.
-// Now we just hand her the top level menu
-// component, the one that contains all the
-// other menus. We've called that allMenus.
+import java.util.Iterator;
+
 public class Waitress {
     MenuComponent allMenus;
 
@@ -11,10 +9,35 @@ public class Waitress {
         this.allMenus = allMenus;
     }
 
-    // All she has to do to print the entire menu
-    // hierarchy - all the menus, and all the menu
-    // items - is call print() on the top level menu.
     public void printMenu() {
         allMenus.print();
+    }
+
+    // The printVegetarianMenu() method
+    // takes the allMenu's composite and 
+    // gets its iterator. That will be our      
+    // CompositeIterator.
+    public void printVegetarianMenu() {
+        Iterator<MenuComponent> iterator = allMenus.createIterator();
+        System.out.println("\nVEGETARIAN MENU\n----");
+        // Iterate through every element of the composite.
+        while (iterator.hasNext()) {
+            MenuComponent menuComponent = (MenuComponent)iterator.next();
+            try {
+                // Call each element's is Vegetarian() method 
+                // and if true, we call its print() method.
+                if (menuComponent.isVegetarian()) {
+                    // print() is only called on MenuItems, 
+                    // never composites. Can you see whu?
+                    menuComponent.print();
+                } 
+            } catch (UnsupportedOperationException e) {
+                // We implemented isVegetarian() on the 
+                // Menus to always throw an exception. If 
+                // that happens we catch the exception, but
+                // continue with our iteration.
+            }
+        }
+        
     }
 } 
