df7385d35d306e848c2f5958038867e7fc1282ed
Mingda-Rui <mingdarui@gmail.com>
2019-05-29 22:46:39 +0100

    GumballMachineTestDrive

---
+++ src/test/java/pers/mingda/designpatterns/chapt10/GumballMachineTestDrive.java
@@ -0,0 +1,58 @@
+package pers.mingda.designpatterns.chapt10;
+
+public class GumballMachineTestDrive {
+    public static void main(String[] args) {
+        // Load it up with
+        // five gumballs total.
+        GumballMachine gumballMachine = new GumballMachine(5);
+
+        // Print out the state of the machine.
+        System.out.println(gumballMachine);
+
+        // Throw a quarter in...
+        gumballMachine.insertQuarter();
+        // Turn the crank; we should get our gumball.
+        gumballMachine.turnCrank();
+
+        // Print out the state of the machine, again.
+        System.out.println(gumballMachine);
+
+        // Throw a quarter in...
+        gumballMachine.insertQuarter();
+        // Ask for it back.
+        gumballMachine.ejectQuarter();
+        // Turn the crank; we shouldn't get our gumball.
+        gumballMachine.turnCrank();
+
+        // Print out the state of the machine, again.
+        System.out.println(gumballMachine);
+
+        // Throw a quarter in...
+        gumballMachine.insertQuarter();
+        // Turn the crank; we should get our gumball
+        gumballMachine.turnCrank();
+        // Throw a quarter in...
+        gumballMachine.insertQuarter();
+        // Turn the crank; we should get our gumball
+        gumballMachine.turnCrank();
+        // Ask for a quarter back we didn't put in
+        gumballMachine.ejectQuarter();
+
+        // Print out the state of the machine, again.
+        System.out.println(gumballMachine);
+
+        // Throw TWO quarters in...
+        gumballMachine.insertQuarter();
+        gumballMachine.insertQuarter();
+        // Turn the crank; we should get our gumball.
+        gumballMachine.turnCrank();
+        // Now for the stress testing...
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        // Print that machine state one more time.
+        System.out.println(gumballMachine);
+    }
+}
