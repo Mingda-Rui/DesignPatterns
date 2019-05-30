5fb866f060acf61cd86f22173dc7e39af33c4522
Mingda-Rui <mingdarui@gmail.com>
2019-05-30 20:53:56 +0100

    State interface in State Pattern

---
+++ src/main/java/pers/mingda/designpatterns/chapt10/State.java
@@ -0,0 +1,10 @@
+package pers.mingda.designpatterns.chapt10;
+
+public interface State {
+
+    public void insertQuarter();
+    public void ejectQuarter();
+    public void turnCrank();
+    public void dispense();
+
+}
