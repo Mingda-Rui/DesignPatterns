f07079670e4f11ff064542fd71f591ca4a640d5f
Mingda-Rui <mingdarui@gmail.com>
2019-06-01 19:38:39 +0100

    GumballMonitor

---
+++ src/main/java/pers/mingda/designpatterns/chapt11/GumballMonitor.java
@@ -0,0 +1,19 @@
+package pers.mingda.designpatterns.chapt11;
+
+public class GumballMonitor {
+    GumballMachine machine;
+
+    // The monitor takes the machine in its
+    // constructor and assigns it to the 
+    // machine instance variable.
+    public GumballMonitor(GumballMachine machine) {
+        this.machine = machine;
+    }
+
+    // Our report method just prints a report with 
+    // location, inventory and the machine's state.
+    public void report() {
+        System.out.println("Gumball Machine: " + machine.getLocation());
+        System.out.println("Current inventory: " + machine.getCount() + " gumballs");
+    }
+}
