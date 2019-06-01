b97502ca2cc77df2abbc43648b641f2ad3a99a9f
Mingda-Rui <mingdarui@gmail.com>
2019-06-01 18:31:35 +0100

    SoldOutState refill support

--- src/main/java/pers/mingda/designpatterns/chapt10/SoldOutState.java
+++ src/main/java/pers/mingda/designpatterns/chapt10/SoldOutState.java
@@ -22,4 +22,13 @@ public class SoldOutState implements State {
     public void dispense() {
         System.out.println("Invoking diepense method in the SoldOutState, Manually Throw IllegalStateException!");
     }
+
+    public void refill(int gumballNumbers) {
+        int currentCount = gumballMachine.getCount();
+        if (currentCount != 0) {
+            System.out.println("Oops, there must be something wrong, SoldOutState should have no gumball left. But refilled the gumballs anyway");
+        }
+        gumballMachine.setCount(currentCount + gumballNumbers);
+        gumballMachine.setState(gumballMachine.getNoQuarterState());
+    }
 }
