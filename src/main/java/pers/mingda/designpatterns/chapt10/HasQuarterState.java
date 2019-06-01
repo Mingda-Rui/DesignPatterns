46c3288ed99f4e6fefa33ab820b21e9d4f846f98
Mingda-Rui <mingdarui@gmail.com>
2019-06-01 14:11:36 +0100

    HasQuarterState v2, support WinnerState

--- src/main/java/pers/mingda/designpatterns/chapt10/HasQuarterState.java
+++ src/main/java/pers/mingda/designpatterns/chapt10/HasQuarterState.java
@@ -1,39 +1,41 @@
 package pers.mingda.designpatterns.chapt10;
 
+import java.util.Random;
+
 public class HasQuarterState implements State {
+    // First we add a random number
+    // generator to generate the 
+    // 10% chance of winning...
+    Random randomWinner = new Random(System.currentTimeMillis());
     GumballMachine gumballMachine;
 
-    // When the state is instantiated 
-    // we pass it a reference to the
-    // GumballMachine. This is used 
-    // to transition the machine to a 
-    // different state.
     public HasQuarterState(GumballMachine gumballMachine) {
         this.gumballMachine = gumballMachine;
     }
 
-    // An inappropriate action for this state.
     public void insertQuarter() {
         System.out.println("You can't insert another quarter");
     }
 
-    // Return the customer's quarter and 
-    // transition back to the NoQuarterState.
     public void ejectQuarter() {
         System.out.println("Quarter returned");
         gumballMachine.setState(gumballMachine.getNoQuarterState());
     }
 
     public void turnCrank() {
-        // When the crank is turned we transition
-        // the machine to the SoldState state by 
-        // calling its setState() method and passing it
-        // the SoldState object. The SoldState object
-        // is retrieved by the getSoldState() getter 
-        // method (there is one of these getter mehtods 
-        // for each state).
+        // ...then we determine if this customer won.
         System.out.println("You turned...");
-        gumballMachine.setState(gumballMachine.getSoldState());
+        int winner = randomWinner.nextInt(10);
+        if ((winner == 0) && (gumballMachine.getCount() > 1)) {
+            // If they won, and there's enough gumballs left for 
+            // them to get two, we go to the WinnerState;
+            // otherwise, we go to the SoldState (just like we
+            // always did).
+            gumballMachine.setState(gumballMachine.getWinnerState());
+        } else {
+            gumballMachine.setState(gumballMachine.getSoldState());
+        }
+        
     }
 
     public void dispense() {
