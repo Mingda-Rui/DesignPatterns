5404a78407262b03854f94018ee5ae36e6c472bf
Mingda-Rui <mingdarui@gmail.com>
2019-06-02 19:12:50 +0100

    GumballMonitorTestDrive

---
+++ src/test/java/pers/mingda/designpatterns/chapt11/GumballMonitorTestDrive.java
@@ -0,0 +1,36 @@
+package pers.mingda.designpatterns.chapt11;
+
+import java.rmi.Naming;
+
+public class GumballMonitorTestDrive {
+
+    public static void main(String[] args) {
+        // Here's all the locations we're gooing to monitor.
+        // We create an array of locaitons, 
+        // one for each machine.
+        String[] location = {"rmi://santafe.mightgumball.com/gumballmachine",
+                            "rmi://boulder.mightgumball.com/gumballmachine",
+                            "rmi://seattle.mightgumball.com/gumballmachine"};
+
+        // We also create an array of monitors.
+        GumballMonitor[] monitor = new GumballMonitor[location.length];
+
+        for (int i = 0; i < location.length; i++) {
+            // Now we need to get a proxy
+            // to each remote machine.
+            try {
+                GumballMachineRemote machine = (GumballMachineRemote) Naming.lookup(location[i]);
+                monitor[i] = new GumballMonitor(machine);
+                System.out.println(monitor[i]);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Then we iterate through each
+        // machine and print out its report.
+        for (int i = 0; i < monitor.length; i++) {
+            monitor[i].report();
+        }
+    }
+}
