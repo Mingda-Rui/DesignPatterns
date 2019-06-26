1cfbe1c36d5f8dcb1de95a4801292da4eaf0c676
Mingda-Rui <mingdarui@gmail.com>
2019-06-26 22:47:34 +0100

    MallardDuck

---
+++ src/main/java/pers/mingda/designpatterns/chapt12/MallardDuck.java
@@ -0,0 +1,8 @@
+package pers.mingda.designpatterns.chapt12;
+
+// Your standard Mallard duck.
+public class MallardDuck implements Quackable {
+    public void quack() {
+        System.out.println("Quack");
+    }
+}
