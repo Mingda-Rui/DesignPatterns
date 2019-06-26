08f68935b47b60a7b21c5226a69ddbb0dd098079
Mingda-Rui <mingdarui@gmail.com>
2019-06-26 22:53:27 +0100

    RubberDuck

---
+++ src/main/java/pers/mingda/designpatterns/chapt12/RubberDuck.java
@@ -0,0 +1,9 @@
+package pers.mingda.designpatterns.chapt12;
+
+public class RubberDuck implements Quackable {
+    public void quack() {
+        // A RubberDuck that makes a 
+        // squeak when it quacks.
+        System.out.println("Squeak");
+    }
+}
