2b15f33eb6232e72ac08aa7934534d0eca7a0a26
Mingda-Rui <mingdarui@gmail.com>
2019-07-10 23:06:44 +0100

    Goose

---
+++ src/main/java/pers/mingda/designpatterns/chapt12/Goose.java
@@ -0,0 +1,9 @@
+package pers.mingda.designpatterns.chapt12;
+
+public class Goose {
+    public void honk() {
+        // A Goose is a honker,
+        // not a quacker.
+        System.out.println("Honk");
+    }
+}
