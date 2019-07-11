9d2d4a7b179c7ad5b93317ad0c0ca100b9a95024
Mingda-Rui <mingdarui@gmail.com>
2019-07-11 22:46:15 +0100

    AbstractDuckFactory

---
+++ src/main/java/pers/mingda/designpatterns/chapt12/AbstractDuckFactory.java
@@ -0,0 +1,13 @@
+package pers.mingda.designpatterns.chapt12;
+
+// We're defining an abstract factory
+// that subclasses will implement to 
+// create differen families.
+public abstract class AbstractDuckFactory {
+
+    // Each method creates one kind of duck.
+    public abstract Quackable createMallardDuck();
+    public abstract Quackable createRedheadDuck();
+    public abstract Quackable createDuckCall();
+    public abstract Quackable createRubberDuck();
+}
