1b55361dec47ace34fc790130aa7fa4a2d3ccc72
Mingda-Rui <mingdarui@gmail.com>
2019-08-10 18:04:36 +0100

    QuackObservale by Observer Pattern

---
+++ src/main/java/pers/mingda/designpatterns/chapt12/observer/QuackObservable.java
@@ -0,0 +1,16 @@
+package pers.mingda.designpatterns.chapt12.observer;
+
+// QuackObervable is the interface
+// that Quackables should implement
+// if they want to be ovserved.
+public interface QuackObservable {
+    // It has a method for registering 
+    // Observers. Any object implementing 
+    // the Obaserver interface can listen
+    // to quacks. We'll define the Observer
+    // interface in a sec.
+    public void registerObserver(Observer observer);
+    // It also has a method for 
+    // notifying the observers.
+    public void notifyObservers();
+}
