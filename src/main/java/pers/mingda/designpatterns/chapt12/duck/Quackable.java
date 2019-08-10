782cb7aaaf8bdd8b0c154cd7ac3a4df7c908605c
Mingda-Rui <mingdarui@gmail.com>
2019-08-10 18:07:50 +0100

    Quackable Interface Extends QuackObservable

--- src/main/java/pers/mingda/designpatterns/chapt12/duck/Quackable.java
+++ src/main/java/pers/mingda/designpatterns/chapt12/duck/Quackable.java
@@ -1,6 +1,10 @@
 package pers.mingda.designpatterns.chapt12.duck;
 
-public interface Quackable {
+import pers.mingda.designpatterns.chapt12.observer.QuackObservable;
+
+// So, we extend the Quackable
+// interface with QuackObserver.
+public interface Quackable extends QuackObservable {
     // Quackables only need to do
     // one thing well: Quack!
     public void quack();
