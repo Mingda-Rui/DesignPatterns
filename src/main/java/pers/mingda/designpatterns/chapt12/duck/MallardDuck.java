4550b4343b09c848f1ef833edd334fa687d06569
Mingda-Rui <mingdarui@gmail.com>
2019-08-11 23:35:57 +0100

    MallardDuck with Observer Pattern

--- src/main/java/pers/mingda/designpatterns/chapt12/duck/MallardDuck.java
+++ src/main/java/pers/mingda/designpatterns/chapt12/duck/MallardDuck.java
@@ -1,8 +1,33 @@
 package pers.mingda.designpatterns.chapt12.duck;
 
+import pers.mingda.designpatterns.chapt12.observer.Observable;
+import pers.mingda.designpatterns.chapt12.observer.Observer;
+
 // Your standard Mallard duck.
 public class MallardDuck implements Quackable {
+    // Each Quackable has an
+    // Observable instance variable.
+    Observable observable;
+
+    // In the constructor, we create an
+    // Observable and pass it a reference
+    // to the MallardDuck object.
+    public MallardDuck() {
+        observable = new Observable(this);
+    }
+
     public void quack() {
         System.out.println("Quack");
+        // When we quack, we need to let the
+        // observers know about it.
+        notifyObservers();
+    }
+
+    public void registerObserver(Observer observer) {
+        observable.registerObserver(observer);
+    }
+
+    public void notifyObservers() {
+        observable.notifyObservers();
     }
 }
